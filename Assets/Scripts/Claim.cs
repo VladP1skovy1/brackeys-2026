a9f8a856aa4e045b5028e44d6388af7c3e6a7695
Vladyslav <piskovijvlad1@gmail.com>
2026-08-26 20:42:46 +0200

    fix: change price type

--- Assets/Scripts/Claim.cs
+++ Assets/Scripts/Claim.cs
@@ -6,6 +6,6 @@ namespace AntiqueShop
     public class Claim
     {
         [field: SerializeField] public string CustomerText { get; private set; }
-        [field: SerializeField] public int AskingPrice { get; private set; }
+        [field: SerializeField] public float AskingPrice { get; private set; }
     }
 }
