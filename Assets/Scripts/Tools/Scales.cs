d380ac5b1e09684ec7b0aba8e2bff8fb1505a87f
vitalii <vitalikvarhanik@gmail.com>
2026-08-28 23:23:46 +0200

    feat: add scales logic

--- Assets/Scripts/Tools/Scales.cs
+++ Assets/Scripts/Tools/Scales.cs
@@ -1,11 +1,24 @@
 using AntiqueShop.Items;
+using AntiqueShop.UI;
+using UnityEngine;
 
 namespace AntiqueShop.Tools
 {
     public class Scales : Tool
     {
+        [SerializeField] private ItemUI itemView;
+        [SerializeField] private ToolReadout readout;
+        [SerializeField] private Transform pan;
+
         protected override void OnToolClick()
         {
+            if (Read() is not float weight)
+            {
+                return;
+            }
+
+            bool onScales = itemView.TogglePosition(pan.position);
+            readout.Show(onScales ? $"{weight:0.##} g" : null);
         }
 
         public override object Read()
