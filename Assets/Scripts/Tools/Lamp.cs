5b5d4fe17ca34e42a551a33625e4795826d28e13
vitalii <vitalikvarhanik@gmail.com>
2026-08-28 22:09:41 +0200

    feat: add magnifier logic to open CloseUpPanel

--- Assets/Scripts/Tools/Lamp.cs
+++ Assets/Scripts/Tools/Lamp.cs
@@ -1,14 +1,16 @@
 using AntiqueShop.Items;
+using AntiqueShop.UI;
+using UnityEngine;
 
 namespace AntiqueShop.Tools
 {
     public class Lamp : Tool
     {
-        protected override void OnToolClick()
-        {
-        }
+        [SerializeField] private ItemUI itemView;
+
+        protected override void OnToolClick() => itemView.Toggle(Read() as Sprite);
 
         public override object Read()
-            => CurrentItem is IUVReactive reactive ? (object)reactive.IsUVReactive : null;
+            => CurrentItem is IUVReactive reactive && reactive.IsUVReactive ? reactive.UVView : null;
     }
 }
