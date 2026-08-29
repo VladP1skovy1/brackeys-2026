63a2673777e3eb4664ad93dfd3231807f45525e8
vitalii <vitalikvarhanik@gmail.com>
2026-08-29 16:13:00 +0200

    add item articles, tutorial text & unity changes

--- Assets/Scripts/Tools/PC.cs
+++ Assets/Scripts/Tools/PC.cs
@@ -1,20 +1,39 @@
-using System;
+using AntiqueShop.Items;
 using UnityEngine;
 
 namespace AntiqueShop.Tools
 {
     public class PC : Tool
     {
-        [Serializable]
-        public class Article
+        public readonly struct Article
         {
-            [field: SerializeField] public string ItemType { get; private set; }
-            [field: SerializeField, TextArea(5, 15)] public string Text { get; private set; }
+            public string Text { get; }
+            public Sprite Image { get; }
+
+            public bool HasContent => !string.IsNullOrEmpty(Text);
+
+            public Article(string text, Sprite image)
+            {
+                Text = text;
+                Image = image;
+            }
         }
 
-        [SerializeField] private Article[] articles;
+        [SerializeField] private Sprite alcoholImage;
+        [SerializeField] private Sprite rockImage;
+        [SerializeField] private Sprite statuetteImage;
+        [SerializeField] private Sprite documentImage;
+        [SerializeField] private Sprite jewelleryImage;
 
-        public Article[] Articles => articles;
+        public Article CurrentArticle => CurrentItem switch
+        {
+            Alcohol => new Article(ItemArticles.AlcoholArticle, alcoholImage),
+            Rock => new Article(ItemArticles.RockArticle, rockImage),
+            Statuette => new Article(ItemArticles.StatuetteArticle, statuetteImage),
+            Document => new Article(ItemArticles.DocumentArticle, documentImage),
+            Jewellery => new Article(ItemArticles.JewelleryArticle, jewelleryImage),
+            _ => default
+        };
 
         protected override void OnToolClick()
         {
